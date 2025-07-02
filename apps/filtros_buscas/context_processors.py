56ee0920faea2c0803d619ab5ea74a9fbe5cb662
José Victor Modesto <j.victormodesto75@gmail.com>
2025-07-02 11:35:44 -0300

    Perfil professor + segurança HTTP

--- apps/filtros_buscas/context_processors.py
+++ apps/filtros_buscas/context_processors.py
@@ -1,11 +1,13 @@
 from apps.user_admin.models import Categoria, Tipos, Instituicao, Campus, Curso
+from django.views.decorators.http import require_GET
 
 # filtros do menu lateral
+@require_GET
 def menu_filtro(request):
-    categorias = Categoria.objects.all()
-    tipos = Tipos.objects.all()
-    instituicoes = Instituicao.objects.all()
-    campus = Campus.objects.all()
-    cursos = Curso.objects.all()
+    categorias = Categoria.objects.order_by('nome_categoria')
+    tipos = Tipos.objects.order_by('nome_tipo')
+    instituicoes = Instituicao.objects.order_by('nome_instituicao')
+    campus = Campus.objects.order_by('nome_campus')
+    cursos = Curso.objects.order_by('nome_curso')
 
     return {'categorias': categorias, 'tipos': tipos, 'instituicoes': instituicoes, 'campus': campus, 'cursos': cursos}
